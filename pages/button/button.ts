831137dd36d952e6c51e2c3c9385206fbd000539
Roland Dillon <roland.dillon7676@gmail.com>
2026-02-25 22:29:32 -0500

    add snazzy signal class

--- pages/button/button.ts
+++ pages/button/button.ts
@@ -2,20 +2,38 @@ const Header: HTMLHeadingElement = document.querySelector("h1")!;
 const PlusButton: HTMLButtonElement = document.querySelector("#plus")!;
 const ResetButton: HTMLButtonElement = document.querySelector("#reset")!;
 
-let count = 0;
+class Signal<T> {
+    private _value: T;
+    private _callbacks: ((newValue: T, oldValue?: T) => void)[] = [];
 
-function updateText() {
-    Header.textContent = `You have clicked the button ${count} times.`;
+    constructor(value: T) {
+        this._value = value;
+    }
+
+    on(f: (newValue: T, oldValue?: T) => void) {
+        this._callbacks.push(f);
+    }
+
+    map(f: (value: T) => T) {
+        this.set(f(this.get()));
+    }
+
+    set(value: T) {
+        const oldValue = this._value;
+        this._value = value;
+
+        for (const cb of this._callbacks) {
+            setTimeout(() => cb(this._value, oldValue), 0)
+        }
+    }
+
+    get(): T { return this._value; }
 }
 
-PlusButton.addEventListener("click", () => {
-    count++;
-    updateText();
-})
+let count = new Signal(0);
+count.on(newValue => Header.textContent = `You have clicked the button ${newValue} times.`)
 
-ResetButton.addEventListener("click", () => {
-    count = 0;
-    updateText();
-})
+PlusButton.addEventListener("click", () => count.map(v => v + 1))
+ResetButton.addEventListener("click", () => count.set(0))
 
-updateText();
+count.set(0);
